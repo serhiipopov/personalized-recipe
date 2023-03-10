26e4bde00db78ba2e0d8b4cea781a5c3494d8bd1
Serhii Popov <sergiiy.popov@gmail.com>
2023-03-10 11:38:15 +0200

    start dev

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import {StyleSheet, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 19,
+    paddingTop: 80,
+  },
+});
+
+export default App;
